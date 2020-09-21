88cfd4f1d34ee3655e019bb7303ab59f667f3da6
qis-andrew <andrew@quasars.com>
2020-09-21 15:42:08 -0400

    init

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//    return view('dashboard');
+//})->name('dashboard');
+
+Route::get('/dashboard', [\App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+Route::resource('cars', \App\Http\Controllers\CarController::class);
+Route::resource('cars.instances', \App\Http\Controllers\CarInstanceController::class);
+Route::get('/user/profile', [\App\Http\Controllers\UserProfileController::class, 'show'])->name('profile.show');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::any('/datatable/ajax/{model}', 'DataTableController@index')->name('portal.datatable.ajax');
 });
