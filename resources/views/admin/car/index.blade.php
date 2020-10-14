f4fd7b456db87e9a61335ff21ce622ad1daa4c78
Aisha <aishamazing@users.noreply.github.com>
2020-10-14 15:07:33 -0400

    updates to instance form and car list view

--- resources/views/admin/car/index.blade.php
+++ resources/views/admin/car/index.blade.php
@@ -1,7 +1,84 @@
+<style>
+ @media screen and (min-width: 768px) {
+  .collapse:not(.show) {
+   display: block!important;
+  }
+  .columnCount {
+   display: flex;
+   flex-wrap: wrap;
+   align-items: flex-start;
+  }
+  .columnCount .carRecord {
+   flex: 0 0 25%;
+   max-width: 25%;
+  }
+  .columnCount .card:not(.carRecord) {
+   flex: 0 0 100%;
+   max-width: 100%;
+  }
+ }
+ .carRecord {
+  position: relative;
+ }
+ .carTitle [class*='border-'] {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  z-index: 10;
+ }
+ .carTitle [class*='border-']:before {
+  border-color: rgba(0, 0, 0, 0) var(--secondary) rgba(0, 0, 0, 0)!important;
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 100px 100px 0;
+ }
+ .carTitle .border-primary:before {
+  border-color: rgba(0, 0, 0, 0) var(--primary) rgba(0, 0, 0, 0)!important;
+ }
+ .carTitle .border-success:before {
+  border-color: rgba(0, 0, 0, 0) var(--success) rgba(0, 0, 0, 0)!important;
+ }
+ .carTitle .border-info:before {
+  border-color: rgba(0, 0, 0, 0) var(--info) rgba(0, 0, 0, 0)!important;
+ }
+ .carTitle .border-warning:before {
+  border-color: rgba(0, 0, 0, 0) var(--warning) rgba(0, 0, 0, 0)!important;
+ }
+ .carTitle .border-danger:before {
+  border-color: rgba(0, 0, 0, 0) var(--danger) rgba(0, 0, 0, 0)!important;
+ }
+ .carTitle [class*='border-'] span {
+  position: absolute;
+  display: block;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  font-weight: 400;
+  width: 100%;
+  height: 100%;
+  line-height: 110px;
+  transform: rotate(45deg) translate(0, -25%);
+ }
+</style>
 <x-app-layout>
  <x-slot name="header">
+  <h1>
+   Inventory
+  </h1>
+  <a class="btn btn-primary" href="{{ route('cars.create') }}">
+   Create Car
+  </a>
+ </x-slot>
+ <div class="mb-3 d-flex flex-nowrap justify-content-between align-items-center">
   <nav aria-label="breadcrumb">
-   <ol class="breadcrumb">
+   <ol class="breadcrumb mb-0">
     <li class="breadcrumb-item">
      <a href="{{ route('dashboard') }}">Dashboard</a>
     </li>
@@ -10,40 +87,206 @@
     </li>
    </ol>
   </nav>
-  <h1>
-   Inventory
-  </h1>
-  <a class="btn btn-primary" href="{{ route('cars.create') }}">
-   Create Car
-  </a>
- </x-slot>
- <div class="card">
-  <div class="card-header">
-   <ul class="nav nav-tabs card-header-tabs" id="inventoryTabs" role="tablist">
-    <li class="nav-item" role="presentation">
-     <a class="nav-link active" id="availCars-tab" data-toggle="tab" href="#availCars" role="tab" aria-controls="availCars" aria-selected="true">Available For Sale</a>
-    </li>
-    <li class="nav-item" role="presentation">
-     <a class="nav-link" id="pendingCars-tab" data-toggle="tab" href="#pendingCars" role="tab" aria-controls="pendingCars" aria-selected="false">Pending Deals</a>
-    </li>
-    <li class="nav-item" role="presentation">
-     <a class="nav-link" id="consignedCars-tab" data-toggle="tab" href="#consignedCars" role="tab" aria-controls="consignedCars" aria-selected="false">On Consignment</a>
-    </li>
-   </ul>
+  <div class="d-md-none">
+   <button class="btn btn-light filterToggle">
+    <span class="fas fa-filter"></span>
+   </button>
   </div>
-  <div class="card-body">
-   <div class="tab-content" id="inventoryTabsContent">
-    <div class="tab-pane fade show active" id="availCars" role="tabpanel" aria-labelledby="availCars-tab">
-     <livewire:car.car-browse />
+  <div class="d-none d-lg-inline">
+   <div class="btn-group btn-group-toggle" data-toggle="buttons" role="group" aria-label="Basic example">
+    <label class="btn btn-secondary">
+     <input type="radio" name="options" id="option3">  <i class="fal fa-align-justify"></i>
+    </label>
+    <label class="btn btn-secondary active">
+     <input type="radio" name="options" id="option1" checked> <i class="fal fa-align-left"></i>
+    </label>
+    <label class="btn btn-secondary">
+     <input type="radio" name="options" id="option2">  <i class="fal fa-align-right"></i>
+    </label>
+   </div>
+   <div class="btn-group btn-group-toggle" data-toggle="buttons" role="group" aria-label="Basic example2">
+    <label class="btn btn-secondary">
+     <input type="radio" name="options" class="cardToggler" id="option4">  <i class="fad fa-th"></i>
+    </label>
+    <label class="btn btn-secondary active">
+     <input type="radio" name="options" class="cardToggler" id="option5" checked>  <i class="fad fa-list"></i>
+    </label>
+   </div>
+  </div>
+ </div><!-- /breadcrumbs and filter view -->
+ <div class="mb-3 d-flex justify-content-between align-items-center flex-wrap">
+  <div class="form-row align-items-center">
+   <div class="col-auto">
+    <label class="mb-0">Sort By:</label>
+   </div>
+   <div class="col">
+    <select class="form-control">
+     <option selected>Name</option>
+     <option>Price Ascending</option>
+    </select>
+   </div>
+  </div>
+  <div class="d-flex">
+   <a href="#" class="btn btn-secondary disabled rounded-0 ">
+    <span class="fal fa-angle-left"></span>
+   </a>
+   <label class="flex-column pl-2 pr-2 justify-content-center align-content-center d-flex mb-0 font-weight-bold">Page 1 of Many</label>
+   <a href="#" class="btn btn-secondary rounded-0">
+    <span class="fal fa-angle-right"></span>
+   </a>
+  </div>
+  <div class="ml-auto mr-auto ml-sm-0 mr-sm-0">
+   <a href="#" class="btn btn-dark btn-sm">Reset Filters</a>
+  </div>
+ </div><!-- /sort by, pagination and reset btn -->
+ <div class="row rearranged">
+  <div class="col-lg-3 filterBlock collapse">
+   <div class="row">
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Years</option>
+     </select>
     </div>
-    <div class="tab-pane fade" id="pendingCars" role="tabpanel" aria-labelledby="pendingCars-tab">
-     <code>Content currently unavailable</code>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Makes</option>
+     </select>
     </div>
-    <div class="tab-pane fade" id="consignedCars" role="tabpanel" aria-labelledby="consignedCars-tab">
-     <code>Content currently unavailable</code>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Models</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Body Styles</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Mileage</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Transmissions</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Fuel Economies</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Conditions</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Locations</option>
+     </select>
+    </div>
+    <div class="col-md-4 col-lg-12 child mb-2">
+     <select name="" id="" class="form-control">
+      <option selected>All Prices</option>
+     </select>
+    </div>
+   </div>
+  </div>
+  <div class="col-lg" id="inventoryChange">
+
+
+   <div class="card border-0 shadow-sm mb-3 carRecord">
+    <div class="d-flex flex-nowrap carTitle">
+     <div class="h4 p-2 mb-0">2020 Lincoln MK Coach Legacy</div>
+     <div class="border-success text-white">
+      <span>New</span>
+     </div>
+    </div>
+    <div class="d-flex flex-nowrap alternator">
+     <div class="d-flex flex-column align-items-center justify-content-center bg-light p-5 text-black-50">Replace with image</div>
+     <div class="p-3 flex-fill small">
+      <div class="form-row flex-nowrap mb-1">
+       <div class="col"><label class="d-block font-weight-bold mb-0">Body Style:</label></div>
+       <div class="col text-truncate">Sport Utility Vehicle</div>
+      </div>
+      <div class="form-row flex-nowrap mb-1">
+       <div class="col"><label class="d-block font-weight-bold mb-0">Drivetrain:</label></div>
+       <div class="col text-truncate">4WD</div>
+      </div>
+      <div class="form-row flex-nowrap mb-1">
+       <div class="col"><label class="d-block font-weight-bold mb-0">Engine:</label></div>
+       <div class="col text-truncate">4.8L V8</div>
+      </div>
+      <div class="form-row flex-nowrap mb-1">
+       <div class="col"><label class="d-block font-weight-bold mb-0">Transmission:</label></div>
+       <div class="col text-truncate">8-Speed Tiptronic</div>
+      </div>
+      <div class="form-row flex-nowrap mb-1">
+       <div class="col"><label class="d-block font-weight-bold mb-0">Mileage:</label></div>
+       <div class="col text-truncate">19,585</div>
+      </div>
+     </div>
+    </div>
+   </div>
+
+   <div class="card">
+    <div class="card-header">
+     <ul class="nav nav-tabs card-header-tabs" id="inventoryTabs" role="tablist">
+      <li class="nav-item" role="presentation">
+       <a class="nav-link active" id="availCars-tab" data-toggle="tab" href="#availCars" role="tab" aria-controls="availCars" aria-selected="true">Available For Sale</a>
+      </li>
+      <li class="nav-item" role="presentation">
+       <a class="nav-link" id="pendingCars-tab" data-toggle="tab" href="#pendingCars" role="tab" aria-controls="pendingCars" aria-selected="false">Pending Deals</a>
+      </li>
+      <li class="nav-item" role="presentation">
+       <a class="nav-link" id="consignedCars-tab" data-toggle="tab" href="#consignedCars" role="tab" aria-controls="consignedCars" aria-selected="false">On Consignment</a>
+      </li>
+     </ul>
+    </div>
+    <div class="card-body">
+     <div class="tab-content" id="inventoryTabsContent">
+      <div class="tab-pane fade show active" id="availCars" role="tabpanel" aria-labelledby="availCars-tab">
+       <livewire:car.car-browse />
+      </div>
+      <div class="tab-pane fade" id="pendingCars" role="tabpanel" aria-labelledby="pendingCars-tab">
+       <code>Content currently unavailable</code>
+      </div>
+      <div class="tab-pane fade" id="consignedCars" role="tabpanel" aria-labelledby="consignedCars-tab">
+       <code>Content currently unavailable</code>
+      </div>
+     </div>
     </div>
    </div>
   </div>
  </div>
 </x-app-layout>
+
+<script>
+ $(document).ready(function(){
+  $('.filterToggle').click(function(event) {
+   $('.filterBlock').toggleClass('show');
+  });
+  $('#option3').click(function(event) {
+   $('.filterBlock').removeClass('col-lg-3').removeClass('order-lg-2').addClass('col-lg-12');
+   $('.filterBlock + div').removeClass('order-lg-1');
+   $('.filterBlock .child').removeClass('col-lg-12');
+  });
+  $('#option1').click(function(event) {
+   $('.filterBlock').addClass('col-lg-3').removeClass('order-lg-2').removeClass('col-lg-12');
+   $('.filterBlock + div').removeClass('order-lg-1');
+   $('.filterBlock .child').addClass('col-lg-12');
+  });
+  $('#option2').click(function(event) {
+   $('.filterBlock').addClass('order-lg-2');
+   $('.filterBlock + div').addClass('order-lg-1');
+   $('.filterBlock .child').addClass('col-lg-12');
+  });
+  $('.cardToggler').click(function(event) {
+   $('#inventoryChange .alternator').toggleClass('flex-column');
+   $('#inventoryChange').toggleClass('columnCount');
+  });
+ })
+</script>
 
--- resources/views/livewire/car/instance-form.blade.php
+++ resources/views/livewire/car/instance-form.blade.php
@@ -1,56 +1,127 @@
-<div>
-  <form id="update-form" wire:submit.prevent="save">
-    <div class="form-label-group">
-      <input type="text" id="received_on" wire:model="received_on" class="form-control @error('received_on') is-invalid @enderror" placeholder="Received On">
-      <label for="received_on">Received On</label>
-      @error('received_on') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+
+<form id="update-form" wire:submit.prevent="save">
+ <div class="flex-sm-nowrap flex-lg-wrap row">
+  <div class="mb-2 mb-sm-3 col-sm col-lg-12 col-xl-5">
+   <div class="row">
+    <div class="mb-2 mb-sm-3 col-md-6 col-lg-3 col-xl-6">
+     <div class="form-label-group d-block position-relative">
+      <input type="text" id="received_on" wire:model="received_on" class="form-control mw-100 mr-0 d-block @error('received_on') is-invalid @enderror" placeholder="Received On">
+      <label class="mw-100 mr-0 mb-0 d-block" for="received_on">Received On</label>
+      @error('received_on') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
+     </div>
     </div>
-    <div class="form-label-group">
-      <input type="text" id="sold_on" wire:model="sold_on" class="form-control @error('sold_on') is-invalid @enderror" placeholder="Sold On">
-      <label for="sold_on">Sold On</label>
-      @error('sold_on') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-6 col-lg-3 col-xl-6">
+     <div class="form-label-group d-block position-relative">
+      <input type="text" id="sold_on" wire:model="sold_on" class="form-control mw-100 mr-0 d-block @error('sold_on') is-invalid @enderror" placeholder="Sold On">
+      <label class="mw-100 mr-0 mb-0 d-block" for="sold_on">Sold On</label>
+      @error('sold_on') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
+     </div>
     </div>
-    
-    <div class="form-label-group">
-      <input type="number" id="cost_orig" wire:model="cost_orig" class="form-control @error('cost_orig') is-invalid @enderror" placeholder="Cost Original">
-      <label for="cost_orig">Cost Original</label>
-      @error('cost_orig') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-6 col-lg-3 col-xl-6">
+     <div class="form-label-group d-block position-relative">
+      <input type="text" id="status" wire:model="status" class="form-control mw-100 mr-0 d-block @error('status') is-invalid @enderror" placeholder="Status">
+      <label class="mw-100 mr-0 mb-0 d-block" for="status">Status</label>
+       @error('status') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
+     </div>
     </div>
-    <div class="form-label-group">
-      <input type="number" id="cost_est" wire:model="cost_est" class="form-control @error('cost_est') is-invalid @enderror" placeholder="Cost Estimate">
-      <label for="cost_est">Cost Estimate</label>
-      @error('cost_est') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-6 col-lg-3 col-xl-6">
+     <div class="form-label-group d-block position-relative">
+      <input type="text" id="status" wire:model="cond" class="form-control mw-100 mr-0 d-block @error('cond') is-invalid @enderror" placeholder="Condition">
+      <label class="mw-100 mr-0 mb-0 d-block" for="cond">Condition</label>
+       @error('cond') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
+     </div>
     </div>
-    <div class="form-label-group">
-      <input type="number" id="cost_repair" wire:model="cost_repair" class="form-control @error('cost_repair') is-invalid @enderror" placeholder="Cost Repair">
-      <label for="cost_repair">Cost Repair</label>
-      @error('cost_repair') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+   </div>
+  </div>
+  <div class="mb-2 mb-sm-3 col-sm-4 col-lg-12 col-xl-7">
+   <div class="row flex-wrap">
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+       <input type="number" id="cost_orig" wire:model="cost_orig" class="form-control mw-100 mr-0 d-block @error('cost_orig') is-invalid @enderror" placeholder="Cost Original">
+       <label class="mw-100 mr-0 mb-0 d-block" for="cost_orig">Original</label>
+      </div>
+     </div>
+     @error('cost_orig') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-    <div class="form-label-group">
-      <input type="number" id="cost_floor" wire:model="cost_floor" class="form-control @error('cost_floor') is-invalid @enderror" placeholder="Cost Floor">
-      <label for="cost_floor">Cost Floor</label>
-      @error('cost_floor') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+       <input type="number" id="cost_est" wire:model="cost_est" class="form-control mw-100 mr-0 d-block @error('cost_est') is-invalid @enderror" placeholder="Cost Estimate">
+       <label class="mw-100 mr-0 mb-0 d-block" for="cost_est">Estimate</label>
+      </div>
+     </div>
+     @error('cost_est') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-    <div class="form-label-group">
-      <input type="number" id="cost_total" wire:model="cost_total" class="form-control @error('cost_total') is-invalid @enderror" placeholder="Cost Total">
-      <label for="cost_total">Cost Total</label>
-      @error('cost_total') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+       <input type="number" id="cost_repair" wire:model="cost_repair" class="form-control mw-100 mr-0 d-block @error('cost_repair') is-invalid @enderror" placeholder="Cost Repair">
+       <label class="mw-100 mr-0 mb-0 d-block" for="cost_repair">Repair</label>
+      </div>
+     </div>
+     @error('cost_repair') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-    <div class="form-label-group">
-      <input type="number" id="cost_sugg" wire:model="cost_sugg" class="form-control @error('cost_sugg') is-invalid @enderror" placeholder="Cost Suggested">
-      <label for="cost_sugg">Cost Suggested</label>
-      @error('cost_sugg') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+       <input type="number" id="cost_floor" wire:model="cost_floor" class="form-control mw-100 mr-0 d-block @error('cost_floor') is-invalid @enderror" placeholder="Cost Floor">
+       <label class="mw-100 mr-0 mb-0 d-block" for="cost_floor">Floor</label>
+      </div>
+     </div>
+     @error('cost_floor') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-    
-    <div class="form-label-group">
-      <input type="text" id="status" wire:model="status" class="form-control @error('status') is-invalid @enderror" placeholder="Status">
-      <label for="status">Status</label>
-      @error('status') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+       <input type="number" id="cost_total" wire:model="cost_total" class="form-control mw-100 mr-0 d-block @error('cost_total') is-invalid @enderror" placeholder="Cost Total">
+       <label class="mw-100 mr-0 mb-0 d-block" for="cost_total">Total</label>
+      </div>
+     </div>
+     @error('cost_total') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-    <div class="form-label-group">
-      <input type="text" id="status" wire:model="cond" class="form-control @error('cond') is-invalid @enderror" placeholder="Condition">
-      <label for="cond">Condition</label>
-      @error('cond') <div class="invalid-feedback"><span>{{ $message }}</span></div> @enderror
+    <div class="mb-2 mb-sm-3 col-md-12 col-lg-4 col-xl-6 col-auto">
+     <div class="input-group d-flex flex-nowrap">
+      <div class="input-group-prepend">
+       <span class="input-group-text">
+        <span class="fal fa-dollar-sign"></span>
+       </span>
+      </div>
+      <div class="form-label-group d-block position-relative">
+      <input type="number" id="cost_sugg" wire:model="cost_sugg" class="form-control mw-100 mr-0 d-block @error('cost_sugg') is-invalid @enderror" placeholder="Cost Suggested">
+      <label class="mw-100 mr-0 mb-0 d-block" for="cost_sugg">Suggested</label>
+      </div>
+     </div>
+     @error('cost_sugg') <div style="z-index:1" class="position-absolute invalid-feedback ml-0 d-flex justify-content-center align-items-center flex-column mt-0"><span>{{ $message }}</span></div> @enderror
     </div>
-  </form>
-</div>
+   </div>
+  </div>
+ </div>
+
+
+</form>
